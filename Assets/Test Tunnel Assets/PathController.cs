9b10a8fbc130f27983a159e3d9da1662246d3cbb
Val6789 <11796016+Val6789@users.noreply.github.com>
2019-12-15 19:32:12 +0100

    Add test scene for example tunnel

---
+++ Assets/Test Tunnel Assets/PathController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Animator>().speed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
