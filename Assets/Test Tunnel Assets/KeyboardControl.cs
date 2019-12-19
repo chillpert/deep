663efef437c0781abfd1e69122f4f73f71dbdb02
Val6789 <11796016+Val6789@users.noreply.github.com>
2019-12-19 12:34:00 +0100

    Test tunnel: add kb controls, add test rocks, change lighting

---
+++ Assets/Test Tunnel Assets/KeyboardControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour
+{
+	//~ float speed = 3.0f;
+	float x = 0.0f;
+	float y = 0.0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey ("up") && transform.position.y < 1.0f) {
+			//~ transform.position += transform.forward * Time.deltaTime * speed;
+			//~ x += Time.deltaTime * speed;
+			transform.Translate(0.0f, 0.1f, 0.0f);
+		}
+		if (Input.GetKey ("down")  && transform.position.y > -1.0f) {
+			//~ transform.position -= transform.forward * Time.deltaTime * speed;
+			//~ x -= Time.deltaTime * speed;
+			transform.Translate(0.0f, -0.1f, 0.0f);
+		}
+		//~ if (Input.GetKey ("left") && transform.position.x > -1.0f) {
+		if (Input.GetKey ("left")) {
+			transform.Translate(-0.1f, 0.0f, 0.0f);
+			//~ rot_y -= speed * Time.deltaTime;
+		}
+		//~ if (Input.GetKey ("right") && transform.position.x < 1.0f) {
+		if (Input.GetKey ("right")) {
+			transform.Translate(0.1f, 0.0f, 0.0f);
+			//~ rot_y += speed * Time.deltaTime;
+		}
+    }
+}
