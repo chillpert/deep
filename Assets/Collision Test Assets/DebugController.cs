1babe4db2808fbae0f826f8f3136a2707799513e
03NN30 <n30.email@gmail.com>
2019-12-20 16:28:58 +0100

    added basic collision example

---
+++ Assets/Collision Test Assets/DebugController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugController : MonoBehaviour
+{
+  [SerializeField]
+  float speedControls;
+  [SerializeField]
+  float constantVelocity;
+
+  Vector3 initialPosition;
+  float lockPos = 0f;
+
+  void Start()
+  {
+    lockPos = 0f;
+    initialPosition = transform.position;
+  }
+
+  void OnCollisionStay(Collision collision)
+  {
+    Debug.Log(collision.collider.name);
+    transform.position = initialPosition;
+  }
+
+  void Update()
+  {
+    transform.Translate(0f, 0f, constantVelocity * Time.deltaTime);
+
+    if (Input.GetKey("up") || Input.GetKey("w"))
+    {
+      transform.Rotate(-speedControls * Time.deltaTime, 0f, 0f);
+    }
+
+    if (Input.GetKey("down") || Input.GetKey("s"))
+    {
+      transform.Rotate(speedControls * Time.deltaTime, 0f, 0f);
+    }
+
+    if (Input.GetKey("left") || Input.GetKey("a"))
+    {
+      transform.Rotate(0f, -speedControls * Time.deltaTime, 0f);
+    }
+
+    if (Input.GetKey("right") || Input.GetKey("d"))
+    {
+      transform.Rotate(0f, speedControls * Time.deltaTime, 0f);
+    }
+
+    // lock z-axis
+    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, lockPos);
+
+    // reset once end of tunnel has been reached
+    if (transform.position.z >= 60f)
+    {
+      transform.position = initialPosition;
+    }
+  }
+}
