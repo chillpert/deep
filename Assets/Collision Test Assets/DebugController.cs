338ac7a3f2fe2e310c617c280260512134189ebb
03NN30 <n30.email@gmail.com>
2019-12-22 21:51:58 +0100

    implemented collision for submarine

--- Assets/Collision Test Assets/DebugController.cs
+++ Assets/Collision Test Assets/DebugController.cs
@@ -8,25 +8,121 @@ public class DebugController : MonoBehaviour
   float speedControls;
   [SerializeField]
   float constantVelocity;
+  [SerializeField]
+  float bounceIntensity;
+  [SerializeField]
+  float turnSpeed;
+  [SerializeField]
+  GameObject camera;
+
+  float bounceLeft = 0f;
+  float bounceRight = 0f;
+  float bounceTop = 0f;
+  float bounceBottom = 0f;
+
+  bool turnLeft = false;
+  bool turnRight = false;
+  bool turnDown = false;
+  bool turnUp = false;
 
   Vector3 initialPosition;
   float lockPos = 0f;
+
+  Rigidbody rb;
+  bool hasCollided = false;
 
   void Start()
   {
     lockPos = 0f;
     initialPosition = transform.position;
+    rb = GetComponent<Rigidbody>();
+    
   }
 
   void OnCollisionStay(Collision collision)
   {
-    Debug.Log(collision.collider.name);
-    transform.position = initialPosition;
-    transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+    //Debug.Log(collision.collider.name);
+
+    StartCoroutine(camera.GetComponent<CameraShake>().Shake());
+
+    if (collision.gameObject.tag == "TunnelTop")
+    {
+      rb.AddForce(-transform.up * bounceIntensity, ForceMode.Impulse);
+      hasCollided = true;
+      turnDown = true;
+    }
+    else if (collision.gameObject.tag == "TunnelBottom")
+    {
+      rb.AddForce(transform.up * bounceIntensity, ForceMode.Impulse);
+      hasCollided = true;
+      turnUp = true;
+    }
+    else if (collision.gameObject.tag == "TunnelLeft")
+    {
+      rb.AddForce(transform.right * bounceIntensity, ForceMode.Impulse);
+      hasCollided = true;
+      turnRight = true;
+    }
+    else if (collision.gameObject.tag == "TunnelRight")
+    {
+      rb.AddForce(-transform.right * bounceIntensity, ForceMode.Impulse);
+      hasCollided = true;
+      turnLeft = true;
+    }
+
+    if (collision.gameObject.tag == "Destructables")
+      Destroy(collision.gameObject);
+
+    if (collision.gameObject.tag == "Finish")
+    {
+      transform.position = initialPosition;
+      transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+    }
   }
 
   void Update()
   {
+    if (hasCollided)
+    {
+      hasCollided = false;
+      rb.velocity = Vector3.zero;
+      rb.angularVelocity = Vector3.zero;
+    }
+
+    if (turnLeft)
+    {
+      if (transform.rotation.eulerAngles.y >= 0f && transform.rotation.eulerAngles.y <= 180f)
+        transform.Rotate(0f, -turnSpeed * Time.deltaTime, 0f);
+      else
+        turnLeft = false;
+    }
+
+    if (turnRight)
+    {
+      if (transform.rotation.eulerAngles.y >= 270f && transform.rotation.eulerAngles.y <= 360f)
+        transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+      else
+        turnRight = false;
+    }
+
+    if (turnDown)
+    {
+      if (transform.rotation.eulerAngles.x >= 0f && transform.rotation.eulerAngles.x <= 180f)
+        turnDown = false;
+      else
+        transform.Rotate(turnSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    if (turnUp)
+    {
+      if (transform.rotation.eulerAngles.x >= 270f && transform.rotation.eulerAngles.x <= 360f)
+        turnUp = false;
+      else
+        transform.Rotate(-turnSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    //Debug.Log(transform.rotation.eulerAngles.x);
+
     transform.Translate(0f, 0f, constantVelocity * Time.deltaTime);
 
     if (Input.GetKey("up") || Input.GetKey("w"))
