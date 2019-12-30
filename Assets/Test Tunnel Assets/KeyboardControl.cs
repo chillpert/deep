a06be1194e705f433a239d324d74e898252e13e6
Val6789 <11796016+Val6789@users.noreply.github.com>
2019-12-30 18:38:08 +0100

    Prepare transition from "tube controls" to story mode

--- Assets/Test Tunnel Assets/KeyboardControl.cs
+++ Assets/Test Tunnel Assets/KeyboardControl.cs
@@ -17,25 +17,32 @@ public class KeyboardControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey ("up") && transform.position.y < 1.0f) {
-			//~ transform.position += transform.forward * Time.deltaTime * speed;
-			//~ x += Time.deltaTime * speed;
-			transform.Translate(0.0f, 0.1f, 0.0f);
-		}
-		if (Input.GetKey ("down")  && transform.position.y > -1.0f) {
-			//~ transform.position -= transform.forward * Time.deltaTime * speed;
-			//~ x -= Time.deltaTime * speed;
-			transform.Translate(0.0f, -0.1f, 0.0f);
-		}
-		//~ if (Input.GetKey ("left") && transform.position.x > -1.0f) {
-		if (Input.GetKey ("left")) {
-			transform.Translate(-0.1f, 0.0f, 0.0f);
-			//~ rot_y -= speed * Time.deltaTime;
-		}
-		//~ if (Input.GetKey ("right") && transform.position.x < 1.0f) {
-		if (Input.GetKey ("right")) {
-			transform.Translate(0.1f, 0.0f, 0.0f);
-			//~ rot_y += speed * Time.deltaTime;
+		if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+		{
+			if (Input.GetKey ("up"))
+			{
+				//~ transform.position += transform.forward * Time.deltaTime * speed;
+				//~ x += Time.deltaTime * speed;
+				transform.Translate(0.0f, 0.1f, 0.0f);
+			}
+			if (Input.GetKey ("down"))
+			{
+				//~ transform.position -= transform.forward * Time.deltaTime * speed;
+				//~ x -= Time.deltaTime * speed;
+				transform.Translate(0.0f, -0.1f, 0.0f);
+			}
+			//~ if (Input.GetKey ("left") && transform.position.x > -1.0f) {
+			if (Input.GetKey ("left"))
+			{
+				transform.Translate(-0.1f, 0.0f, 0.0f);
+				//~ rot_y -= speed * Time.deltaTime;
+			}
+			//~ if (Input.GetKey ("right") && transform.position.x < 1.0f) {
+			if (Input.GetKey ("right"))
+			{
+				transform.Translate(0.1f, 0.0f, 0.0f);
+				//~ rot_y += speed * Time.deltaTime;
+			}
 		}
     }
 }
