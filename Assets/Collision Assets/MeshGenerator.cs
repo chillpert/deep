699a462acb4d63208f7f7d61ab21ac9558d014dd
chillpert <dev@christianhilpert.com>
2020-01-15 14:15:49 +0100

    flipped normals of generated meshes

--- Assets/Collision Assets/MeshGenerator.cs
+++ Assets/Collision Assets/MeshGenerator.cs
@@ -21,12 +21,30 @@ public class MeshGenerator : MonoBehaviour
     ++index;
 
     // calculate forward direction of bridge
-    Vector3 newForward = next.transform.GetChild(0).transform.GetChild(0).position - transform.GetChild(0).transform.GetChild(2).position;
+    Vector3 newForward = next.transform.position - transform.position;
+    //Vector3 newForward = next.transform.GetChild(0).transform.GetChild(0).position - transform.GetChild(0).transform.GetChild(2).position;
 
     container.transform.forward = newForward;
 
     for (int i = 0; i < 4; ++i)
     {
+      string name = "__UNDEF__";
+      switch (i)
+      {
+        case 0:
+          name = "Bottom";
+          break;
+        case 1:
+          name = "Top";
+          break;
+        case 2:
+          name = "Left";
+          break;
+        case 3:
+          name = "Right";
+          break;
+      }
+
       newVertices = new Vector3[]
       {
         transform.GetChild(i).transform.GetChild(2).position,
@@ -47,25 +65,42 @@ public class MeshGenerator : MonoBehaviour
       };
 
       Mesh newMesh = new Mesh();
-      newMesh.vertices = newVertices;
-      newMesh.uv = newUV;
-      newMesh.triangles = newTriangles;
 
-      string name = "__UNDEF__";
-      switch (i)
+      // flip normals and uvs if mesh is top or right
+      if (i == 1 || i == 3)
       {
-        case 0:
-          name = "Bottom";
-          break;
-        case 1:
-          name = "Left";
-          break;
-        case 2:
-          name = "Top";
-          break;
-        case 3:
-          name = "Right";
-          break;
+        int[] reversedTriangles = newTriangles;
+
+        int numpolies = reversedTriangles.Length / 3;
+        for (int t = 0; t < numpolies; t++)
+        {
+          int tribuffer = reversedTriangles[t * 3];
+          reversedTriangles[t * 3] = reversedTriangles[(t * 3) + 2];
+          reversedTriangles[(t * 3) + 2] = tribuffer;
+        }
+
+        Vector2[] flippedUV = newUV;
+        for (int uvnum = 0; uvnum < flippedUV.Length; uvnum++)
+        {
+          flippedUV[uvnum] = new Vector2(1 - flippedUV[uvnum].x, flippedUV[uvnum].y);
+        }
+
+        Vector3[] flippedNormals = newMesh.normals;
+        for (int normalsnum = 0; normalsnum < flippedNormals.Length; normalsnum++)
+        {
+          flippedNormals[normalsnum] = -flippedNormals[normalsnum];
+        }
+
+        newMesh.vertices = newVertices;
+        newMesh.uv = flippedUV;
+        newMesh.triangles = reversedTriangles;
+        newMesh.normals = flippedNormals;
+      }
+      else
+      {
+        newMesh.vertices = newVertices;
+        newMesh.uv = newUV;
+        newMesh.triangles = newTriangles;
       }
 
       GameObject bridge = new GameObject(name);
