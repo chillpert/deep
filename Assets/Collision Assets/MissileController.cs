8094f64181bb6796a4d680e89285a8f480f84368
Val6789 <11796016+Val6789@users.noreply.github.com>
2020-01-17 11:50:36 +0100

    Add missile prefab and smoke effects

---
+++ Assets/Collision Assets/MissileController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0.0f, -1.0f, 0.0f); // This speed should be coherent with the exhaust particle speed
+        
+    }
+    
+	// TODO Check collision with wall (delete self), obstacle (delete obstacle)
+}
