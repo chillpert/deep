b89bc2bc964ed89ec3390058e465bdf8fe86c7b8
chillpert <dev@christianhilpert.com>
2020-04-01 17:57:55 +0200

    deleted unncessary files

---
+++ Assets/Scripts/BuoyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuoyController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+		// Stub method
+		Debug.Log("Buoy collision");
+	}
+}
---
+++ Assets/Scripts/MissileController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileController : MonoBehaviour
+{
+	[SerializeField]
+	public GameObject trailFX;
+	[SerializeField]
+	public GameObject explosionFX;
+	[SerializeField]
+	float controlSpeed = 1.5f;
+
+	float startTime;
+	
+  void Start()
+  {
+		Destroy(transform.root.gameObject, 10);
+		startTime = Time.timeSinceLevelLoad;
+	}
+
+  void Update()
+  {
+		transform.Rotate(-Input.GetAxis("Controller Vertical") * Time.deltaTime * controlSpeed, Input.GetAxis("Controller Horizontal") * Time.deltaTime * controlSpeed, 0f, Space.World);
+		transform.Translate(-10.0f * Time.deltaTime, 0f, 0f); // This speed should be coherent with the exhaust particle speed
+  }
+    
+	void OnCollisionStay(Collision collision)
+	{
+		if(collision.gameObject.tag == "Destructables")
+		{
+			Object.Destroy(collision.gameObject);
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+		}
+
+		/*
+		if(collision.gameObject.name.Contains("CavePart"))
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+		else
+			explode();
+		*/
+		explode();
+	}
+	
+	void explode()
+	{
+		GetComponent<AudioSource>().Play();
+		explosionFX.SetActive(true);
+		trailFX.SetActive(false);
+		transform.localScale = new Vector3(0, 0, 0);
+		Destroy(transform.root.gameObject, 2);
+	}
+}
