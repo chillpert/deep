80dd82da3be77776e9cc771b21cefe325069c751
chillpert <dev@christianhilpert.com>
2020-04-01 18:07:14 +0200

    cleaned up project folder structure

---
+++ Assets/Scripts/KeyboardControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class KeyboardControl : MonoBehaviour
+{
+  void Start()
+  {
+        
+  }
+
+  void Update()
+  {
+		if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+		{
+			if (Input.GetKey ("up"))
+			{
+				//~ transform.position += transform.forward * Time.deltaTime * speed;
+				//~ x += Time.deltaTime * speed;
+				transform.Translate(0.0f, 0.1f, 0.0f);
+			}
+			if (Input.GetKey ("down"))
+			{
+				//~ transform.position -= transform.forward * Time.deltaTime * speed;
+				//~ x -= Time.deltaTime * speed;
+				transform.Translate(0.0f, -0.1f, 0.0f);
+			}
+			//~ if (Input.GetKey ("left") && transform.position.x > -1.0f) {
+			if (Input.GetKey ("left"))
+			{
+				transform.Translate(-0.1f, 0.0f, 0.0f);
+				//~ rot_y -= speed * Time.deltaTime;
+			}
+			//~ if (Input.GetKey ("right") && transform.position.x < 1.0f) {
+			if (Input.GetKey ("right"))
+			{
+				transform.Translate(0.1f, 0.0f, 0.0f);
+				//~ rot_y += speed * Time.deltaTime;
+			}
+		}
+  }
+}
