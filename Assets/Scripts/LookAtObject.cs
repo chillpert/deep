28a536120561c88375c17259f44d7e984aa010ef
chillpert <dev@christianhilpert.com>
2020-04-18 14:20:32 +0200

    added ray casting for cave interaction

---
+++ Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+  private void Update()
+  {
+    int layerMask = 1 << 10;
+
+    RaycastHit hit;
+
+    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+    {
+      // hit
+    }
+  }
+}
